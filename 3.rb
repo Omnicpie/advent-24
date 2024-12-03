991ce3ebb2c9a02a8d0f16033bcca9f5a7d46d50
Ry Anderson <ryananderson1999@hotmail.co.uk>
2024-12-03 20:47:31 +0000

    Day 2 Multiply matches and Sum

--- 3.rb
+++ 3.rb
@@ -1,3 +1,6 @@
 VALID_REGEX = /mul\(\d{1,3},\d{1,3}\)/
 
-puts File.read('./inputs/3.txt').scan(VALID_REGEX)
+matches = File.read('./inputs/3.txt').scan(VALID_REGEX)
+
+res = matches.map { |string| string.scan(/\d/).map { |str| str.to_i }.inject(:*) }.inject(:+)
+puts res
