fff98b7ef7ac6ead72c3d238fe2872a5377315bd
Ry Anderson <ryananderson1999@hotmail.co.uk>
2024-12-03 22:17:50 +0000

    DAY 3 rework regex to use capturing group with options, WORKED

--- 3.rb
+++ 3.rb
@@ -1,24 +1,26 @@
 VALID_REGEX = /mul\(\d{1,3},\d{1,3}\)/
-START_STRING_REGEX = /.*?don't\(\)/
-MIDDLE_STRING_REGEX = /do\(\).*?don't\(\)/
-END_STRING_REGEX_PART_1 = /^.*?\)\(t'nod/
-END_STRING_REGEX_PART_2 = /do\(\).*?$/
+ENABLING_REGEX = /(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))/
+NUMBER_REGEX = /\d+/
 
-matches = File.read('./inputs/3.txt').scan(VALID_REGEX)
-middle = File.read('./inputs/3.txt').scan(MIDDLE_STRING_REGEX)
-middle_matches = middle.map{|str| str.scan(VALID_REGEX)}.flatten
-start_matches = START_STRING_REGEX.match(File.read('./inputs/3.txt')).to_s.scan(VALID_REGEX)
-end_matches = END_STRING_REGEX_PART_2.match(END_STRING_REGEX_PART_1.match(File.read('./inputs/3.txt').reverse).to_s.reverse).to_s.scan(VALID_REGEX)
+basic_matches = File.read('./inputs/3.txt').scan(VALID_REGEX)
+matches_with_on_off = File.read('./inputs/3.txt').scan(ENABLING_REGEX).flatten
 
-puts "start matches: #{start_matches.length}"
-puts "  mid matches: #{middle_matches.length}"
-puts "  end matches: #{end_matches.length}"
+count = basic_matches.map { |string| string.scan(NUMBER_REGEX).map { |str| str.to_i }.inject(:*) }.inject(:+)
 
+enabled = true
+mul = []
 
-start_count = start_matches.map { |string| string.scan(/\d+/).map { |str| str.to_i }.inject(:*) }.inject(:+)
-end_count = end_matches.map { |string| string.scan(/\d+/).map { |str| str.to_i }.inject(:*) }.inject(:+)
-middle_count = middle_matches.map { |string| string.scan(/\d+/).map { |str| str.to_i }.inject(:*) }.inject(:+)
-count = matches.map { |string| string.scan(/\d+/).map { |str| str.to_i }.inject(:*) }.inject(:+)
+for item in matches_with_on_off do
+  if item.match(/do\(\)/)
+    enabled = true
+  elsif item.match(/don't\(\)/)
+    enabled = false
+  else
+    if enabled
+      mul.push(item.scan(NUMBER_REGEX).map { |str| str.to_i }.inject(:*))
+    end
+  end
+end
 
 puts "part 1: #{count}"
-puts "part 2: #{start_count + middle_count + end_count}"
+puts "part 2: #{mul.inject(:+)}"
