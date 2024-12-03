f3035235d6dd821a477a085126aeae7a25bdfe1b
Ry Anderson <ryananderson1999@hotmail.co.uk>
2024-12-03 20:48:19 +0000

    Day 3 fix regex typo

--- 3.rb
+++ 3.rb
@@ -2,5 +2,5 @@ VALID_REGEX = /mul\(\d{1,3},\d{1,3}\)/
 
 matches = File.read('./inputs/3.txt').scan(VALID_REGEX)
 
-res = matches.map { |string| string.scan(/\d/).map { |str| str.to_i }.inject(:*) }.inject(:+)
+res = matches.map { |string| string.scan(/\d+/).map { |str| str.to_i }.inject(:*) }.inject(:+)
 puts res
